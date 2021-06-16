48c067e346104d46a1681f688be19eddaa36e910
Tyler Rockwood <rockwotj@gmail.com>
2021-06-16 00:45:22 -0500

    Cleanup

---
+++ java/playmaker/fieldpieces/CurvedLine.java
@@ -0,0 +1,159 @@
+package playmaker.fieldpieces;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.QuadCurve2D;
+
+public class CurvedLine extends Line {
+  private int x1;
+  private int y1;
+  private int x2;
+  private int y2;
+  private int xCrtl;
+  private int yCrtl;
+  private double xCrtlPercent;
+  private double yCrtlPercent;
+  private QuadCurve2D temp;
+
+  public CurvedLine(int x, int y, double width, double height) {
+    super(x, y, width, height);
+    this.x1 = x;
+    this.y1 = y;
+    this.x2 = x;
+    this.y2 = y;
+    this.temp = new QuadCurve2D.Double();
+    this.x1Percent = (x / width);
+    this.x2Percent = (x / width);
+    this.y1Percent = (y / height);
+    this.y2Percent = (y / height);
+  }
+
+  public CurvedLine(
+      int x1, int y1, int xCrtl, int yCrtl, int x2, int y2, double width, double height) {
+    super(x1, y1, width, height);
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.bounds = new QuadCurve2D.Double(x1, y1, xCrtl, yCrtl, x2, y2);
+    this.x1Percent = (x1 / width);
+    this.x2Percent = (x2 / width);
+    this.y1Percent = (y1 / height);
+    this.y2Percent = (y2 / height);
+    this.xCrtlPercent = (xCrtl / width);
+    this.yCrtlPercent = (yCrtl / height);
+  }
+
+  public void drawOn(Graphics2D g2) {
+    Stroke original = g2.getStroke();
+    if (this.highlighted) {
+      g2.setColor(Color.white);
+      g2.setStroke(new BasicStroke(3.5F));
+      g2.draw(this.bounds);
+    }
+    g2.setStroke(new BasicStroke(2.0F));
+    g2.setColor(Color.black);
+    g2.draw(this.bounds);
+    g2.setStroke(original);
+  }
+
+  public void moveTo(int x, int y, double width, double height) {
+    QuadCurve2D.Double line = (QuadCurve2D.Double) this.bounds;
+    if (Point2D.Double.distance(x, y, this.anchorX, this.anchorY) > 1.0D) {
+      int xDiff = x - this.anchorX;
+      int yDiff = y - this.anchorY;
+      line.setCurve(
+          line.x1 + xDiff,
+          line.y1 + yDiff,
+          line.ctrlx + xDiff,
+          line.ctrly + yDiff,
+          line.x2 + xDiff,
+          line.y2 + yDiff);
+      this.anchorX = x;
+      this.anchorY = y;
+      this.x1 = ((int) line.x1);
+      this.y1 = ((int) line.y1);
+      this.y2 = ((int) line.y2);
+      this.x2 = ((int) line.x2);
+      this.xCrtl = ((int) line.ctrlx);
+      this.yCrtl = ((int) line.ctrly);
+      this.x1Percent = (line.x1 / width);
+      this.x2Percent = (line.x2 / width);
+      this.y1Percent = (line.y1 / height);
+      this.y2Percent = (line.y2 / height);
+      this.xCrtlPercent = (line.ctrlx / width);
+      this.yCrtlPercent = (line.ctrly / height);
+    }
+  }
+
+  public void moveTail(int x, int y, double width, double height) {
+    this.x2 = x;
+    this.y2 = y;
+    Line2D.Double line = (Line2D.Double) this.bounds;
+    line.setLine(line.x1, line.y1, x, y);
+    this.x2Percent = (line.x2 / width);
+    this.y2Percent = (line.y2 / height);
+  }
+
+  public void setControl(int x, int y, double width, double height) {
+    this.xCrtl = x;
+    this.yCrtl = y;
+    this.bounds = this.temp;
+    QuadCurve2D.Double line = (QuadCurve2D.Double) this.bounds;
+    line.setCurve(this.x1, this.y1, this.xCrtl, this.yCrtl, this.x2, this.y2);
+    this.xCrtlPercent = (line.ctrlx / width);
+    this.yCrtlPercent = (line.ctrly / height);
+  }
+
+  public boolean contains(int x, int y) {
+    if ((this.bounds instanceof QuadCurve2D.Double)) {
+      QuadCurve2D.Double line = (QuadCurve2D.Double) this.bounds;
+      if (line.getFlatness() < 25.0D) {
+        return Line2D.Double.ptSegDist(line.x1, line.y1, line.x2, line.y2, x, y) < 25.0D;
+      }
+      return line.contains(x, y);
+    }
+    Line2D.Double line = (Line2D.Double) this.bounds;
+    return line.ptSegDist(x, y) < 5.0D;
+  }
+
+  public String toString() {
+    QuadCurve2D.Double line = (QuadCurve2D.Double) this.bounds;
+    return "CurvedLine|"
+        + line.x1
+        + "|"
+        + line.y1
+        + "|"
+        + line.ctrlx
+        + "|"
+        + line.ctrly
+        + "|"
+        + line.x2
+        + "|"
+        + line.y2;
+  }
+
+  public void resize(int height, int width) {
+    this.x1 = ((int) (this.x1Percent * width));
+    this.y1 = ((int) (this.y1Percent * height));
+    this.x2 = ((int) (this.x2Percent * width));
+    this.y2 = ((int) (this.y2Percent * height));
+    this.xCrtl = ((int) (this.xCrtlPercent * width));
+    this.yCrtl = ((int) (this.yCrtlPercent * height));
+    QuadCurve2D.Double line = (QuadCurve2D.Double) this.bounds;
+    line.setCurve(this.x1, this.y1, this.xCrtl, this.yCrtl, this.x2, this.y2);
+  }
+}
+
+/*
+ * Location: D:\Software\Mine\CCHS-Playmaker-master\CCHS-Playmaker-master\2.0\
+ * FootballPlayMaker.jar
+ *
+ * Qualified Name: GUI.CurvedLine
+ *
+ * JD-Core Version: 0.7.0.1
+ */
---
+++ java/playmaker/fieldpieces/TextBox.java
@@ -0,0 +1,168 @@
+package playmaker.fieldpieces;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Stroke;
+import java.awt.font.TextLayout;
+import java.awt.geom.Point2D;
+import playmaker.Main;
+
+public class TextBox extends FieldPieces {
+  private Font font;
+  private TextLayout textBox;
+  private int x;
+  private int y;
+  private String text;
+  private int size;
+  private String fontStyle;
+  private Color textColor;
+  private double xPercent;
+  private double yPercent;
+
+  public TextBox(int x, int y, Graphics2D g2, String string, double width, double height) {
+    this.size = Main.defaultFontSize;
+    this.fontStyle = Main.defaultFontStyle;
+    this.font = new Font(this.fontStyle, 1, this.size);
+    this.text = string;
+    this.textBox = new TextLayout(this.text, this.font, g2.getFontRenderContext());
+    this.x = x;
+    this.y = y;
+    this.textColor = Main.defaultTextColor;
+    this.xPercent = (x / width);
+    this.yPercent = (y / height);
+  }
+
+  public TextBox(
+      int x,
+      int y,
+      Graphics2D g2,
+      String text,
+      String font,
+      int size,
+      String textColor,
+      double width,
+      double height) {
+    setFontAndSizeAndText(font, text, size, g2);
+    this.x = x;
+    this.y = y;
+    setFontColor(textColor);
+    this.xPercent = (x / width);
+    this.yPercent = (y / height);
+  }
+
+  public void drawOn(Graphics2D g2) {
+    if (this.highlighted) {
+      Stroke original = g2.getStroke();
+      g2.setColor(Color.white);
+      Rectangle outline = this.textBox.getPixelBounds(null, this.x, this.y);
+      outline.setBounds(this.x - 2, outline.y - 2, outline.width + 5, outline.height + 3);
+      g2.draw(outline);
+      g2.setStroke(original);
+    }
+    g2.setColor(this.textColor);
+    this.textBox.draw(g2, this.x, this.y);
+  }
+
+  public void moveTo(int x, int y, double width, double height) {
+    if (Point2D.Double.distance(x, y, this.anchorX, this.anchorY) > 1.0D) {
+      int xDiff = x - this.anchorX;
+      int yDiff = y - this.anchorY;
+      this.x += xDiff;
+      this.y += yDiff;
+      this.anchorX = x;
+      this.anchorY = y;
+      this.xPercent = (this.x / width);
+      this.yPercent = (this.y / height);
+    }
+  }
+
+  public boolean contains(int x, int y) {
+    return this.textBox.getPixelBounds(null, this.x, this.y).contains(x, y);
+  }
+
+  public void setFontAndSizeAndText(String font, String text, int size, Graphics2D g2) {
+    this.text = text;
+    this.size = size;
+    this.fontStyle = font;
+    this.font = new Font(this.fontStyle, 1, this.size);
+    this.textBox = new TextLayout(this.text, this.font, g2.getFontRenderContext());
+  }
+
+  public String getText() {
+    return this.text;
+  }
+
+  public String getSize() {
+    return String.valueOf(this.size);
+  }
+
+  public String getFont() {
+    return this.fontStyle;
+  }
+
+  public void setFontStyle(String fontStyle, Graphics2D g2) {
+    this.fontStyle = fontStyle;
+    this.font = new Font(this.fontStyle, 1, this.size);
+    this.textBox = new TextLayout(this.text, this.font, g2.getFontRenderContext());
+  }
+
+  public String getFontColor() {
+    if (this.textColor.equals(Color.BLACK)) {
+      return "Black";
+    }
+    if (this.textColor.equals(Color.WHITE)) {
+      return "White";
+    }
+    if (this.textColor.equals(Color.BLUE)) {
+      return "Blue";
+    }
+    return "Red";
+  }
+
+  public void setFontColor(String color) {
+    if (color.equals("Black")) {
+      this.textColor = Color.BLACK;
+    } else if (color.equals("White")) {
+      this.textColor = Color.WHITE;
+    } else if (color.equals("Blue")) {
+      this.textColor = Color.BLUE;
+    } else {
+      this.textColor = Color.RED;
+    }
+  }
+
+  public String toString() {
+    return "TextBox|"
+        + this.x
+        + "|"
+        + this.y
+        + "|"
+        + this.text
+        + "|"
+        + this.fontStyle
+        + "|"
+        + this.size
+        + "|"
+        + getFontColor();
+  }
+
+  public Rectangle getBounds() {
+    return this.textBox.getPixelBounds(null, this.x, this.y);
+  }
+
+  public void resize(int height, int width) {
+    this.x = ((int) (this.xPercent * width));
+    this.y = ((int) (this.yPercent * height));
+  }
+}
+
+/*
+ * Location: D:\Software\Mine\CCHS-Playmaker-master\CCHS-Playmaker-master\2.0\
+ * FootballPlayMaker.jar
+ *
+ * Qualified Name: GUI.TextBox
+ *
+ * JD-Core Version: 0.7.0.1
+ */
